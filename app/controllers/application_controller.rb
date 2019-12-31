1f4930f8c0b7de0e7091c13b0555b0beb6b93a7b
Chaya Deaver <chayadeaver@gmail.com>
2019-12-31 12:42:29 -0500

    edit welcome page and removed css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,13 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def current_user
+      @user ||= User.find_by_id(session[:user_id]) 
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
 end
