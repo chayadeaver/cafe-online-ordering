43392eeaa7aed91644cf42b9ac1982bf6e1d1960
Chaya Deaver <chayadeaver@gmail.com>
2020-01-07 20:34:40 -0500

    removed sqlite3

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,6 @@ ENV["RACK_ENV"] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
 
 require './app/controllers/application_controller'
 # require 'sinatra/flash'
