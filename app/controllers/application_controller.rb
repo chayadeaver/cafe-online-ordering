c989bd89f3bec177664cb730f64db4a34c812fd3
Chaya Deaver <chayadeaver@gmail.com>
2020-01-08 08:09:17 -0500

    removed line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     register Sinatra::Flash
-    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+    set :session_secret, ENV.fetch('SESSION_SECRET')
   end
 
   get "/" do
