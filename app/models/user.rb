fa776f3a53f15ca34eeb740f3d3a448613f50653
Chaya Deaver <chayadeaver@gmail.com>
2020-01-10 13:30:53 -0500

    Add validation for email in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :email, presence: true
     validates :email, uniqueness: true
+    validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
     has_many :orders
 end
