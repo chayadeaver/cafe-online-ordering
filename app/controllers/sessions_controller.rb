4cac12046c5377e60674f3c2a70225ef1161e2e4
Chaya Deaver <chayadeaver@gmail.com>
2020-01-14 11:54:40 -0500

    Added sessions controller and moved login there

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+    get "/login" do 
+        if logged_in?
+        redirect to "/"
+        end
+        erb :"/users/login"
+    end
+    
+    post "/login" do
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            flash.next[:message] = "You are successfully logged in."
+            redirect to "/"
+        else
+            if !user
+                user = User.new(email: params[:email], password: params[:password])
+                user.valid?
+            end
+        flash[:error] = user.errors.full_messages
+        redirect to "/login"
+        end
+    end
+
+    get "/logout" do
+        if logged_in?
+            session.clear
+            flash[:message] = "You have successfully logged out. Have a nice day!"
+        else
+            redirect to "/"
+        end
+        redirect "/login"
+    end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,40 +23,8 @@ class UsersController < ApplicationController
     
   end
 
-  get "/login" do 
-    if logged_in?
-      redirect to "/"
-    end
-    erb :"/users/login"
-  end
-
-  post "/login" do
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash.next[:message] = "You are successfully logged in."
-      redirect to "/"
-    else
-      if !user
-        user = User.new(email: params[:email], password: params[:password])
-        user.valid?
-      end
-      flash[:error] = user.errors.full_messages
-        # binding.pry
-        redirect to "/login"
-    end
-
-    
-  end
+  
 
   # DELETE: /users/5/delete
-  get "/logout" do
-    if logged_in?
-      session.clear
-      flash[:message] = "You have successfully logged out. Have a nice day!"
-    else
-      redirect to "/"
-    end
-    redirect "/login"
-  end
+  
 end
